684235f513e6b9225037a3ed1d6b246ea87cf095
Pavel Dvoinos <pavel.dvoinos@transferwise.com>
2020-11-10 03:52:29 +0200

    tests for case insensitive

--- comparison.sh
+++ comparison.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+shopt -s nocasematch
+
 pr_comparison() {
   if [[ "$GITHUB_PULL_REQUEST_EVENT_BODY" =~ $PULL_REQUEST_CONTAINS_PATTERN && ! "$GITHUB_PULL_REQUEST_EVENT_BODY" =~ $PULL_REQUEST_NOT_CONTAINS_PATTERN ]]; then
     BODY_IS_OK=true
@@ -9,11 +11,11 @@ pr_comparison() {
 
   if $BODY_IS_OK
   then
-    echo "GITHUB_PULL_REQUEST_EVENT_BODY matches"
+    echo "PR BODY matches"
 
     return
   else
-    echo "GITHUB_PULL_REQUEST_EVENT_BODY not matches"
+    echo "PR BODY not matches"
 
     false
   fi
--- tests/test.sh
+++ tests/test.sh
@@ -35,14 +35,21 @@ assert_eq 1 $? "$ERR"
 echo $((i+=1))
 echo "Body matches pattern - OK"
 GITHUB_PULL_REQUEST_EVENT_BODY="Hello, fixing [TICKET] and [OTHER]"
-PULL_REQUEST_CONTAINS_PATTERN="[TICKET]"
+PULL_REQUEST_CONTAINS_PATTERN="\[TICKET\]"
+pr_comparison
+assert_eq 0 $? "$ERR"
+
+echo $((i+=1))
+echo "Body matches pattern case insensitive - OK"
+GITHUB_PULL_REQUEST_EVENT_BODY="Fixing [Ticket]"
+PULL_REQUEST_CONTAINS_PATTERN="\[TICKET\]"
 pr_comparison
 assert_eq 0 $? "$ERR"
 
 echo $((i+=1))
 echo "Body matches pattern and NOT pattern - OK"
 GITHUB_PULL_REQUEST_EVENT_BODY="Hello, fixing [TICKET] and [OTHER]"
-PULL_REQUEST_CONTAINS_PATTERN="[TICKET]"
+PULL_REQUEST_CONTAINS_PATTERN="\[TICKET\]"
 PULL_REQUEST_NOT_CONTAINS_PATTERN=".*Why is this PR necessary?.*"
 pr_comparison
 assert_eq 0 $? "$ERR"
@@ -50,7 +57,7 @@ assert_eq 0 $? "$ERR"
 echo $((i+=1))
 echo "Body not matches pattern and matches NOT pattern - FAIL"
 GITHUB_PULL_REQUEST_EVENT_BODY="This is default text.\n Why is this PR necessary?\nAdd text."
-PULL_REQUEST_CONTAINS_PATTERN="[TICKET]"
+PULL_REQUEST_CONTAINS_PATTERN="\[TICKET\]"
 PULL_REQUEST_NOT_CONTAINS_PATTERN=".*Why is this PR necessary?.*"
 pr_comparison
 assert_eq 1 $? "$ERR"
